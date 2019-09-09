b29000edba7ef2c689fea866d9e525e756ba6927
odinokun <odinokun@gmail.com>
2019-09-09 19:13:13 +0300

    did top section size

--- src/js/modules/top-section.js
+++ src/js/modules/top-section.js
@@ -2,8 +2,10 @@ module.exports = function() {
 
   // begin top section
   $('.top-section__description-btn').on('click', function () {
-    $(this).addClass('hide');
-    $(this).siblings('.top-section__description-text').addClass('active');
+    $(this).fadeOut();
+    $(this).siblings('.top-section__description-text').slideDown();
+    // $(this).addClass('hide');
+    // $(this).siblings('.top-section__description-text').addClass('active');
   })
   // end top section
 
